babd3b1de3a37850716498ac18c94aa49982169f
danisanchezmc <danisanchezmc@hotmail.com>
2025-06-06 13:46:13 +0200

    controlador para crear paquetes turisticos nuevos

--- EBaldapa/src/java/Controller/crearPaqueteAction.java
+++ EBaldapa/src/java/Controller/crearPaqueteAction.java
@@ -5,7 +5,13 @@
  */
 package Controller;
 
+import WS.PaqueteTuristicoWS;
 import com.opensymphony.xwork2.ActionSupport;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import modelo.Destino;
+import modelo.PaqueteTuristico;
+import modelo.ProveedorServicios;
 
 /**
  *
@@ -13,11 +19,94 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class crearPaqueteAction extends ActionSupport {
     
+    private String descripcion;
+    private int duracion;
+    private String titulo;
+    private float precio;
+    private Date fechaSalida;
+    private Destino destino;
+    private ProveedorServicios proveedor;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Destino getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Destino destino) {
+        this.destino = destino;
+    }
+
+    public ProveedorServicios getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(ProveedorServicios proveedor) {
+        this.proveedor = proveedor;
+    }
+    
     public crearPaqueteAction() {
     }
     
     public String execute() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        PaqueteTuristico paqueteT = new PaqueteTuristico();
+        
+        paqueteT.setDescripcion("desc");
+        paqueteT.setDuracion(50);
+        paqueteT.setTitulo("prueba");
+        paqueteT.setPrecio(80);
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date fecha_parseada = format.parse("1987-05-27");
+        paqueteT.setFechaSalida(fecha_parseada);
+
+        //paqueteT.setIdDestino(destino);
+        //paqueteT.setIdProveedor(proveedor);
+        
+        PaqueteTuristicoWS paqueteTuristico = new PaqueteTuristicoWS();
+        Object obj_paquete = paqueteT;
+        paqueteTuristico.create_XML(obj_paquete);
+        
+        return SUCCESS;
     }
     
 }
