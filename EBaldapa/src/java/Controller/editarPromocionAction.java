6a18a60f69b072e159f0964d8769839c1e9236b7
pfalrod <146184922+Pablichi@users.noreply.github.com>
2025-06-08 23:15:03 +0200

    editarPromocion
    Añadida la funcionalidad para editar promociones ya existentes

---
+++ EBaldapa/src/java/Controller/editarPromocionAction.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import WS.PaqueteTuristicoWS;
+import WS.PromocionWS;
+import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import javax.ws.rs.core.GenericType;
+import modelo.PaqueteTuristico;
+import modelo.Promocion;
+
+/**
+ *
+ * @author pabli
+ */
+public class editarPromocionAction extends ActionSupport {
+    
+    private int idPromocion;
+    private String nombrePromocion;
+    private float valorPromocion;
+    private String fechaInicioPromocion;
+    private String fechaFinPromocion;
+    private String descripcionPromocion;
+    private String tipoPromocion;
+    
+    private int idPaquete;
+
+    public int getIdPromocion() {
+        return idPromocion;
+    }
+
+    public void setIdPromocion(int idPromocion) {
+        this.idPromocion = idPromocion;
+    }
+
+    public String getNombrePromocion() {
+        return nombrePromocion;
+    }
+
+    public void setNombrePromocion(String nombrePromocion) {
+        this.nombrePromocion = nombrePromocion;
+    }
+
+    public float getValorPromocion() {
+        return valorPromocion;
+    }
+
+    public void setValorPromocion(float valorPromocion) {
+        this.valorPromocion = valorPromocion;
+    }
+
+    public String getFechaInicioPromocion() {
+        return fechaInicioPromocion;
+    }
+
+    public void setFechaInicioPromocion(String fechaInicioPromocion) {
+        this.fechaInicioPromocion = fechaInicioPromocion;
+    }
+
+    public String getFechaFinPromocion() {
+        return fechaFinPromocion;
+    }
+
+    public void setFechaFinPromocion(String fechaFinPromocion) {
+        this.fechaFinPromocion = fechaFinPromocion;
+    }
+
+    public String getDescripcionPromocion() {
+        return descripcionPromocion;
+    }
+
+    public void setDescripcionPromocion(String descripcionPromocion) {
+        this.descripcionPromocion = descripcionPromocion;
+    }
+
+    public String getTipoPromocion() {
+        return tipoPromocion;
+    }
+
+    public void setTipoPromocion(String tipoPromocion) {
+        this.tipoPromocion = tipoPromocion;
+    }
+
+    public int getIdPaquete() {
+        return idPaquete;
+    }
+
+    public void setIdPaquete(int idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+    
+    
+    
+    public editarPromocionAction() {
+    }
+    
+    public String execute() throws Exception {
+        Promocion promocionT = new Promocion();
+        
+        promocionT.setId(idPromocion);
+        promocionT.setNombrePromocion(nombrePromocion);
+        promocionT.setTipoDescuento(tipoPromocion);
+        promocionT.setValorDescuento(valorPromocion);
+        promocionT.setDescripcion(descripcionPromocion);
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        
+        Date fechaInicio_parseada = format.parse(fechaInicioPromocion);
+        promocionT.setFechaInicio(fechaInicio_parseada);
+        
+        Date fechaFin_parseada = format.parse(fechaFinPromocion);
+        promocionT.setFechaFinalizacion(fechaFin_parseada);
+        
+        PromocionWS promocionDAO = new PromocionWS();
+        
+        PaqueteTuristicoWS paqueteDAO = new PaqueteTuristicoWS();
+                GenericType<PaqueteTuristico> genericType = new GenericType<PaqueteTuristico>(){
+        };
+        PaqueteTuristico data = new PaqueteTuristico();
+        data = (PaqueteTuristico) paqueteDAO.find_XML(genericType, Integer.toString(idPaquete));     
+         
+
+        promocionT.setIdPaquete(data);
+
+        Object obj_paquete = promocionT;
+        promocionDAO.edit_XML(obj_paquete, String.valueOf(idPromocion));
+        
+        
+        GenericType<List<Promocion>> genericTypeListado = new GenericType<List<Promocion>>(){
+        };
+        List <Promocion> dataListado = new ArrayList<Promocion>();
+        dataListado = (List<Promocion>) promocionDAO.findAll_XML(genericTypeListado);     
+         
+        Map <String, Object> session = ActionContext.getContext().getSession();
+        session.put("listaPromociones", dataListado);
+        
+        return SUCCESS;
+    }
+    
+}
